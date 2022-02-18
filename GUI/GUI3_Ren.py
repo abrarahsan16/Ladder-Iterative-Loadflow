c398d7c18773e486f2133312486ff2efdf5bafdf
rehnuba <36093302+rehnuba@users.noreply.github.com>
2022-02-18 11:38:36 -0500

    This has the meeting minutes and GUI update
    Sorry not all the file got pushed at once

---
+++ GUI/GUI3_Ren.py
@@ -0,0 +1,22 @@
+#Save File GUI
+import PySimpleGUI as sg
+
+sg.theme('SandyBeach')
+layout = [[
+    sg.InputText(key='File to Save', default_text='filename', enable_events=True),
+    sg.InputText(key='Save As', do_not_clear=False, enable_events=True, visible=False),
+    sg.FileSaveAs(initial_folder='/tmp')
+]]
+window = sg.Window('Save File', layout)
+
+while True:
+    event, values = window.Read()
+    print("event:", event, "values: ",values)
+    if event is None or event == 'Exit':
+        break
+    elif event == 'Save As':
+        filename = values['Save As']
+        if filename:
+            window['File to Save'].update(value=filename)
+
+window.close()
